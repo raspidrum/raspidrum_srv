f198a88a867841463078fbe1a00aa7c33d4da0d5
raspidrum <raspidrum@gmail.com>
2025-04-28 12:54:10 +0300

    added test for aument preset controls and layers

--- internal/app/preset/load_preset_test.go
+++ internal/app/preset/load_preset_test.go
@@ -3,6 +3,7 @@ package preset
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	midi "github.com/raspidrum-srv/internal/app/mididevice"
 	m "github.com/raspidrum-srv/internal/model"
 )
@@ -34,14 +35,166 @@ func Test_augmentFromInstrument(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
+		want    m.KitPreset
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "wo layers",
+			args: args{
+				pst: &m.KitPreset{
+					Instruments: []m.PresetInstrument{
+						{
+							Instrument: m.InstrumentRef{
+								CfgMidiKey: "KEYKICK",
+								Controls: map[string]m.Control{
+									"volume": {CfgKey: "KICKV"},
+								},
+							},
+							MidiKey: "kick1",
+							Controls: map[string]m.PresetControl{
+								"volume": {
+									MidiCC: 30,
+								},
+							},
+						},
+					},
+				},
+				mididevs: []midi.MIDIDevice{
+					&MockMMIDIDevice{},
+				},
+			},
+			want: m.KitPreset{
+				Instruments: []m.PresetInstrument{
+					{
+						Instrument: m.InstrumentRef{
+							CfgMidiKey: "KEYKICK",
+							Controls: map[string]m.Control{
+								"volume": {CfgKey: "KICKV"},
+							},
+						},
+						MidiKey:  "kick1",
+						MidiNote: 36,
+						Controls: map[string]m.PresetControl{
+							"volume": {
+								MidiCC: 30,
+								CfgKey: "KICKV",
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "with layers",
+			args: args{
+				pst: &m.KitPreset{
+					Instruments: []m.PresetInstrument{
+						{
+							Instrument: m.InstrumentRef{
+								Controls: map[string]m.Control{
+									"pan":   {CfgKey: "RI17P"},
+									"pitch": {CfgKey: "RI17T"},
+								},
+								Layers: map[string]m.Layer{
+									"bell": {
+										CfgMidiKey: "RI17BKEY",
+										Controls: map[string]m.Control{
+											"volume": {CfgKey: "RI17BV"},
+										},
+									},
+									"edge": {
+										CfgMidiKey: "RI17EKEY",
+										Controls: map[string]m.Control{
+											"volume": {CfgKey: "RI17EV"},
+										},
+									},
+								},
+							},
+							Controls: map[string]m.PresetControl{
+								"pan":   {MidiCC: 105},
+								"pitch": {MidiCC: 16},
+							},
+							Layers: map[string]m.PresetLayer{
+								"bell": {
+									MidiKey: "ride1_bell",
+									Controls: map[string]m.PresetControl{
+										"volume": {MidiCC: 104},
+									},
+								},
+								"edge": {
+									MidiKey: "ride1_edge",
+									Controls: map[string]m.PresetControl{
+										"volume": {MidiCC: 103},
+									},
+								},
+							},
+						},
+					},
+				},
+				mididevs: []midi.MIDIDevice{
+					&MockMMIDIDevice{},
+				},
+			},
+			want: m.KitPreset{
+				Instruments: []m.PresetInstrument{
+					{
+						Instrument: m.InstrumentRef{
+							Controls: map[string]m.Control{
+								"pan":   {CfgKey: "RI17P"},
+								"pitch": {CfgKey: "RI17T"},
+							},
+							Layers: map[string]m.Layer{
+								"bell": {
+									CfgMidiKey: "RI17BKEY",
+									Controls: map[string]m.Control{
+										"volume": {CfgKey: "RI17BV"},
+									},
+								},
+								"edge": {
+									CfgMidiKey: "RI17EKEY",
+									Controls: map[string]m.Control{
+										"volume": {CfgKey: "RI17EV"},
+									},
+								},
+							},
+						},
+						Controls: map[string]m.PresetControl{
+							"pan":   {MidiCC: 105, CfgKey: "RI17P"},
+							"pitch": {MidiCC: 16, CfgKey: "RI17T"},
+						},
+						Layers: map[string]m.PresetLayer{
+							"bell": {
+								MidiKey:    "ride1_bell",
+								CfgMidiKey: "RI17BKEY",
+								MidiNote:   53,
+								Controls: map[string]m.PresetControl{
+									"volume": {MidiCC: 104, CfgKey: "RI17BV"},
+								},
+							},
+							"edge": {
+								MidiKey:    "ride1_edge",
+								CfgMidiKey: "RI17EKEY",
+								MidiNote:   51,
+								Controls: map[string]m.PresetControl{
+									"volume": {MidiCC: 103, CfgKey: "RI17EV"},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := augmentFromInstrument(tt.args.pst, tt.args.mididevs); (err != nil) != tt.wantErr {
+			preset := tt.args.pst
+			if err := augmentFromInstrument(preset, tt.args.mididevs); (err != nil) != tt.wantErr {
 				t.Errorf("augmentFromInstrument() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(tt.want, *preset); diff != "" {
+				t.Errorf("MakeGatewayInfo() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
